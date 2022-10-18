b7d152bba176bbc78ecd5f3cb7a9288b915ebe93
OisouNcs <98240933+OisouNcs@users.noreply.github.com>
2022-10-17 22:43:01 -0300

    Update en.lua

--- resources/[core]/garages/locales/en.lua
+++ resources/[core]/garages/locales/en.lua
@@ -1,28 +1,33 @@
 local Translations = {
     error = {
         no_vehicles = "Não há veículos neste local!",
-        not_impound = "Seu veículo não está apreendido",
+        not_impound = "Seu veículo não está em impacto",
         not_owned = "Este veículo não pode ser armazenado",
-        not_correct_type = "Você não pode armazenar este tipo de veículo aqui",
+        not_correct_type = "Você não pode armazenar esse tipo de veículo aqui",
         not_enough = "Dinheiro insuficiente",
         no_garage = "Nenhum",
-        vehicle_occupied = "Você não pode armazenar este veículo, pois não está vazio",
+        too_far_away = "Muito longe de um estacionamento",
+        occupied = "O estacionamento já está ocupado",
+        all_occupied = "Todos os lugares de estacionamento estão ocupados",
+        no_vehicle = "Não há veículo para estacionar",
+        no_house_keys = "Você não tem as chaves para esta garagem da casa",
     },
     success = {
         vehicle_parked = "Veículo armazenado",
     },
     menu = {
         header = {
-            house_car = "Casa Garagem %{value}",
-            public_car = "Garagem Pública %{value}",
-            public_sea = "Barco Público %{value}",
+            house_garage = "Garagem da casa",
+            house_car = "Garagem da casa %{value}",
+            public_car = "Garagem pública %{value}",
+            public_sea = "Boathouse pública %{value}",
             public_air = "Hangar público %{value}",
-            job_car = "Oficina de Trabalho %{value}",
-            job_sea = "Estaleiro de Emprego %{value}",
-            job_air = "Hangar de Trabalho %{value}",
-            gang_car = "Garagem de Gang %{value}",
-            gang_sea = "Barco de gangues %{value}",
-            gang_air = "Hangar de gangues %{value}",
+            job_car = "Jó Garage %{value}",
+            job_sea = "Jó Boathouse %{value}",
+            job_air = "Hangar de emprego %{value}",
+            gang_car = "Garagem de gangues %{value}",
+            gang_sea = "Boathouse de gangue %{value}",
+            gang_air = "Hangar de gangue %{value}",
             depot_car = "depósito %{value}",
             depot_sea = "depósito %{value}",
             depot_air = "depósito %{value}",
@@ -32,27 +37,21 @@ local Translations = {
         },
         leave = {
             car = "⬅ Sair da garagem",
-            sea = "⬅ Sair da casa de barcos",
+            sea = "⬅ Sair da Boathouse",
             air = "⬅ Sair do Hangar",
+            job = "⬅ Sair da Garage"
         },
         text = {
             vehicles = "Ver veículos armazenados!",
-            depot = "Placa: %{value}<br>Combustível: %{value2} | Motor: %{value3} | Corpo: %{value4}",
-            garage = "Estado: %{value}<br>Combustível: %{value2} | Motor: %{value3} | Corpo: %{value4}",
+            depot = "Placa: %{value}<br>Gasolina: %{value2} | Motor: %{value3} | Corpo: %{value4}",
+            garage = "State: %{value}<br>Gasolina: %{value2} | Motor: %{value3} | Corpo: %{value4}",
         }
     },
     status = {
-        out = "Fora",
-        garaged = "Garagem",
-        impound = "Apreendido pela polícia",
+        out = "Out",
+        garaged = "Garaged",
+        impound = "Impounded",
     },
-    info = {
-        car_e = "E - Garagem",
-        sea_e = "E - Ancoradouro",
-        air_e = "E - Hangar",
-        park_e = "E - Armazenar Veículo",
-        house_garage = "Garagem da casa",
-    }
 }
 
 Lang = Locale:new({
